23fac2bf331fffba6532ab059e5563dc623fbcb1
shailup50 <shailendra@cruxcreativesolutions.com>
2025-12-16 10:56:40 +0530

    blog dtails page design and home page design2

--- src/app/components/BlogSlider.js
+++ src/app/components/BlogSlider.js
@@ -8,8 +8,6 @@ import Image from "next/image";
 import { useState } from "react";
 
 
-
-
 const BlogSlider = ({ title = "Mindful Nutrition: Our Blogs", blogs = [] }) => {
     const [swiperRef, setSwiperRef] = useState(null);
 
